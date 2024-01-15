45f65a3a773a24f7b41d1fa39193ba231e691d79
Jeff Fox <jeffaveryfox@gmail.com>
2024-01-15 15:59:10 -0600

    remix config

--- remix.config.js
+++ remix.config.js
@@ -1,23 +1,23 @@
-  /** @type {import('@remix-run/dev').AppConfig} */
+/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
 	// serverBuildTarget: "vercel",
-  publicPath: "/build/",
-  serverBuildPath: "api/index.js",
-//   serverMainFields: ["main, module"],
-  serverModuleFormat: "cjs",
-  serverPlatform: "node",
-  serverMinify: false,
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "api/index.js",
-  // publicPath: "/build/",
-//   routes(defineRoutes) {
-//     // uses the v1 convention, works in v1.15+ and v2
-//     return createRoutesFromFolders(defineRoutes);
-//   },
-};
+	publicPath: '/build/',
+	serverBuildPath: 'api/index.js',
+	//   serverMainFields: ["main, module"],
+	serverModuleFormat: 'cjs',
+	serverPlatform: 'node',
+	serverMinify: false,
+	// When running locally in development mode, we use the built in remix
+	// server. This does not understand the vercel lambda module format,
+	// so we default back to the standard build output.
+	server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
+	ignoredRouteFiles: ['**/.*'],
+	// appDirectory: "app",
+	// assetsBuildDirectory: "public/build",
+	// serverBuildPath: "api/index.js",
+	// publicPath: "/build/",
+	//   routes(defineRoutes) {
+	//     // uses the v1 convention, works in v1.15+ and v2
+	//     return createRoutesFromFolders(defineRoutes);
+	//   },
+}
