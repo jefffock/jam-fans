fe77ed8c4b8855d20eef6abf10dfdfa5c45a424f
Jeff Fox <jeffaveryfox@gmail.com>
2024-01-15 17:04:12 -0600

    updated server config to v2 conventions

--- remix.config.js
+++ remix.config.js
@@ -5,17 +5,17 @@ module.exports = {
 	// serverBuildPath: 'api/index.js',
 	//   serverMainFields: ["main, module"],
 	serverModuleFormat: 'cjs',
-	appDirectory: 'app',
-	assetsBuildDirectory: 'public/build',
+	// appDirectory: 'app',
+	// assetsBuildDirectory: 'public/build',
 	// serverPlatform: 'node',
-	publicPath: '/build/',
-	serverBuildPath: 'build/index.js',
+	// publicPath: '/build/',
+	// serverBuildPath: 'build/index.js',
 
 	// serverMinify: false,
 	// When running locally in development mode, we use the built in remix
 	// server. This does not understand the vercel lambda module format,
 	// so we default back to the standard build output.
-	server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
+	// server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
 	ignoredRouteFiles: ['**/.*'],
 	// appDirectory: "app",
 	// assetsBuildDirectory: "public/build",
