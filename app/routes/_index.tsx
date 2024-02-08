7ecb2eeef84c716efc42533baa7c054bebaaeaaa
Jeff Fox <jeffaveryfox@gmail.com>
2024-02-08 01:28:54 -0600

    moved iframe to index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,7 +4,6 @@ import { useLoaderData } from '@remix-run/react'
 import { useEffect, useRef, useState } from 'react'
 import EntityListContainer from '~/components/EntityListContainer'
 import EntityListHeader from '~/components/EntityListHeader'
-import Iframe from '~/components/Iframe'
 import SiteStats from '~/components/SiteStats'
 import useFilterEffects from '~/hooks/use-filter-effects'
 import useFilteredMusicalEntities from '~/hooks/use-filtered-musical-entities'
@@ -182,6 +181,7 @@ export default function Index() {
 
 	const [iframeUrl, setIframeUrl] = useState('')
 	const [formattedIframeUrl, setFormattedIframeUrl] = useState('')
+	const isRelisten = formattedIframeUrl?.includes('relist')
 
 	useEffect(() => {
 		let reformattedLink
@@ -323,9 +323,16 @@ export default function Index() {
 				/>
 			</EntityListContainer>
 			{iframeOpen && formattedIframeUrl && (
-				<>
-					<Iframe formattedIframeUrl={formattedIframeUrl} closeIframe={closeIframe} />
-				</>
+				<div
+					className={`z-20 fixed bottom-0 left-0 pt-1 pr-1 m-0 ${
+						isRelisten ? 'w-full h-1/3' : 'max-w-80  max-h-50 md:mb-0'
+					} drop-shadow-sm rounded-tr-xl md:rounded-bl-none bg-black flex flex-col`}
+				>
+					<button onClick={closeIframe} className="text-left text-white ml-2">
+						Close &#215;
+					</button>
+					<iframe src={formattedIframeUrl} title={'listen'} height={'100%'} width={'100%'}></iframe>
+				</div>
 			)}{' '}
 		</div>
 	)
