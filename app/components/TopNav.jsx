421a7a5fd777f4787d922f26b5d84c93fc06d649
Jeff Fox <jeffaveryfox@gmail.com>
2024-02-10 08:16:45 -0600

    temp disable create account

--- app/components/TopNav.jsx
+++ app/components/TopNav.jsx
@@ -100,12 +100,12 @@ export default function TopNav({ profile }) {
 												>
 													login
 												</a>
-												<a
+												{/* <a
 													href="/join"
 													className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 inline-flex px-1 border-b-2 text-sm font-medium mr-8"
 												>
 													create an account
-												</a>
+												</a> */}
 											</div>
 										)}
 										<div className="-mr-2 flex items-center">
